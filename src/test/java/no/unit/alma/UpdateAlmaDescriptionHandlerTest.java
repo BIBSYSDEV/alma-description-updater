24e0d923b22b835f2970a85f2e136f417fdee17b
Eirik Eide Saur <eisa@unit.no>
2021-03-05 09:28:04 +0100

    Fixes checkstyles in test classes

--- src/test/java/no/unit/alma/UpdateAlmaDescriptionHandlerTest.java
+++ src/test/java/no/unit/alma/UpdateAlmaDescriptionHandlerTest.java
@@ -6,7 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import javax.ws.rs.core.Response;
 
