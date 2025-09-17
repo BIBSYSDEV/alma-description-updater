8df1dd1cd7658bc50c18c822069e139c600ce59b
Tor Arne Torgersen <6398417+torarnet@users.noreply.github.com>
2025-09-17 18:35:57 +0200

    ts-44: remove redundant methods

--- src/main/java/no/unit/alma/AlmaClient.java
+++ src/main/java/no/unit/alma/AlmaClient.java
@@ -18,7 +18,8 @@ public class AlmaClient {
     private static final Logger logger = LoggerFactory.getLogger(AlmaClient.class);
 
     private static final int DEFAULT_RETRY_INTERVAL_IN_SECONDS = 3;
-    private static final int MAX_RETRIES = 3;
+    private static final int MAX_ATTEMPTS = 3;
+    private static final int FIRST_ATTEMPT = 1;
     private static final String RETRIEVED_BIB_RECORD = "Successfully retrieved Bib record with mms_id {} from Alma";
     private static final String UPDATED_BIB_RECORD = "Successfully updated Bib record with mms_id {} in Alma";
     private static final String ATTEMPT_FAILED_WITH_STATUS_CODE = "Attempt {} failed with status code: {}";
@@ -39,40 +40,15 @@ public class AlmaClient {
     }
 
     /**
-     * A method that sends a get request to ALMA.
-     * @param mmsId The mms id needed to specify which post to retrieve.
-     * @return A http response mirroring the response from the get request sent to ALMA.
-     * @throws InterruptedException When something goes wrong.
-     * @throws IOException When something goes wrong.
-     */
-    private HttpResponse<String> getBibRecordFromAlma(String mmsId) throws InterruptedException, IOException {
-        return connection.sendGet(mmsId);
-    }
-
-    /**
-     * A method that sends a put request to ALMA.
-     * @param mmsId The mms id needed to specify which post to update.
-     * @param updatedXml The string which we want to update the post with.
-     * @return A http response mirroring the response from the put request sent to ALMA.
-     * @throws InterruptedException When something goes wrong.
-     * @throws IOException When something goes wrong.
-     */
-    private HttpResponse<String> putBibRecordInAlma(String mmsId, String updatedXml)
-        throws InterruptedException, IOException {
-
-        return connection.sendPut(mmsId, updatedXml);
-    }
-
-    /**
      * Method to retry GET-calls to ALMA, sleeps for 3 seconds before retrying.
      * @param mmsId For identifying the record in ALMA.
      * @return HttpResponse with the ALMA response or null if failing.
      */
     public HttpResponse<String> getBibRecordFromAlmaWithRetries(String mmsId) throws InterruptedException {
-        for (int attempt = 1; attempt <= MAX_RETRIES; attempt++) {
+        for (int attempt = 1; attempt <= MAX_ATTEMPTS; attempt++) {
             addDelayOnNewAttempts(attempt);
             try {
-                var almaResponse = getBibRecordFromAlma(mmsId);
+                var almaResponse = connection.sendGet(mmsId);
 
                 if (isSuccessful(almaResponse)) {
                     logger.info(RETRIEVED_BIB_RECORD, mmsId);
@@ -97,10 +73,10 @@ public class AlmaClient {
     public HttpResponse<String> putBibRecordInAlmaWithRetries(String mmsId, String updatedRecord)
         throws InterruptedException {
 
-        for (int attempt = 1; attempt <= MAX_RETRIES; attempt++) {
+        for (int attempt = 1; attempt <= MAX_ATTEMPTS; attempt++) {
             addDelayOnNewAttempts(attempt);
             try {
-                var almaResponse = putBibRecordInAlma(mmsId, updatedRecord);
+                var almaResponse = connection.sendPut(mmsId, updatedRecord);
 
                 if (isSuccessful(almaResponse)) {
                     logger.info(UPDATED_BIB_RECORD, mmsId);
@@ -117,7 +93,7 @@ public class AlmaClient {
     }
 
     private void addDelayOnNewAttempts(int attempt) throws InterruptedException {
-        if (attempt > 0) {
+        if (attempt > FIRST_ATTEMPT) {
             TimeUnit.SECONDS.sleep(retryIntervalInSeconds);
         }
     }
